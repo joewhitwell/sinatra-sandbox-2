29c667c6870d04cedff019e9579fa782784c383b
tbscanlon <thomas.scanlon@outlook.com>
2017-06-20 12:56:20 +0100

    Add views

--- app.rb
+++ app.rb
@@ -21,17 +21,5 @@ get '/top_secret' do
 end
 
 get '/cat' do
-
-  "<div class=fun-border> sdfghj  </div>
-
-<p>You can edit the CSS below to experiment with border styles!</p>"
-
-"<img style= 'border: 100px dotted red;' src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>"
-
-
+  erb(:index)
 end
-
-
-# border: 1px;
-# border: 2px dotted;
-# border: medium dashed green;
